d0ba72b861dd9bb0de5be83d5340703b47402393
Rodrigo <rodrihercar24@gmail.com>
2025-07-15 09:33:41 -0600

    cambios finales del programa (merge) y comente los primeros prints

--- actividad 1.py
+++ actividad 1.py
@@ -1,16 +1,22 @@
+"""
 print("hola primer GIT")
 print("hola primer GIT")
 print("hola primer GIT")
 print("commit and push")
 print("nuevo print de prueba")
 print("rama de desarrollo moneril")
+"""
+
 
 while True:
     print("----resumen de la clase 4 de programacion avanzada----\n"
           "1. clase 4")
     resumen = int(input("escriba la opcion que quiere: "))
     if resumen == 1:
-        print("se aprendio a usar git dento de Pycharm \n"
+        print("resumen: se aprendio a usar git dento de Pycharm \n"
               "se guardo de forma local y en la nube con (commit y commit and push)\n"
-              "se creo una rama principal y otra de desarrollo para hacer cambios sin afectar a la principal")
+              "se creo una rama principal y otra de desarrollo para hacer cambios sin afectar a la principal\n"
+              "se puede pasar el trabajo copiando el url y subir en una Vesion control")
         break
+    else:
+        print("Error no escribio una opcion incorrecta vuelva a intentar" )
