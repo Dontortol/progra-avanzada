dabbc1df66382232ded85494dbf956dc42f4f70c
Rodrigo <rodrihercar24@gmail.com>
2025-07-15 09:22:39 -0600

    creacion del ciclo y el input del programa

--- actividad 1.py
+++ actividad 1.py
@@ -4,3 +4,7 @@ print("hola primer GIT")
 print("commit and push")
 print("nuevo print de prueba")
 print("rama de desarrollo moneril")
+
+while True:
+    print("\n----resumen de la clase 4 de programacion avanzada----")
+    resumen = int(input("escriba 1 para resumen de la clase: "))
